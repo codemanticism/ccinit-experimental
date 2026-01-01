fe09e9b0adee688ca866d9da9025353cd9ab11ad
Codemanticism <gabibkamkins@gmail.com>
2026-01-01 14:59:33 -0300

    commit

--- libraries/calc.c
+++ libraries/calc.c
@@ -1,5 +1,3 @@
-/*/number.c*/
-
 enum Operation{
 	ADD,
 	SUBTRACT,
@@ -9,15 +7,16 @@ enum Operation{
 };
 //Works, but it actually edits what it receives, so it must be editable, which can be made using the array.
 struct option_float process(char* arr, char** ptr, char* changed_ptr){
-	u32 layer = 0;
 	float result = 0;
 	*changed_ptr = 0;
 	enum Operation op = UNDEFINED;
+	printf("hey");
 	for(u32 i = 0; arr[i] != '\0'; i++){
 		switch(arr[i]){
 			case ' ':
 				break;
 			case '+':
+				printf("+");
 				op = ADD;
 				break;
 			case '-':
@@ -32,45 +31,39 @@ struct option_float process(char* arr, char** ptr, char* changed_ptr){
 			case ')':
 				*ptr = &arr[i];
 				*changed_ptr = 1;
-				break;												
+				struct option_float flt_;
+				flt_.unactivated = 0;
+				flt_.number = result;
+				return flt_;												
 			case '(':
-				char* changed_ptr;
-				char** to_write_to;
-				struct option_float opt = process_float(&arr[i], to_write_to, changed_ptr);
+				char* changed_ptr = malloc(1);
+				char** to_write_to = malloc(1);
+				struct option_float opt_ = process(&arr[i + 1], to_write_to, changed_ptr);
 				if(*changed_ptr){
-					i = *to_write_to;
+					arr = *to_write_to;
+					i = 0;
 				}
-				if(opt.unactivated == 1){
-					opt.unactivated = 1;
-					return opt;
+				if(opt_.unactivated == 1){
+					opt_.unactivated = 1;
+					return opt_;
 				}
-				arr[i] = character;
 				switch(op){
 					case UNDEFINED:
-						result = the_number;
+						result = opt_.number;
 						break;
 					case ADD:
-						result += the_number;
+						result += opt_.number;
 						break;					
 					case SUBTRACT:
-						result -= the_number;					
+						result -= opt_.number;					
 						break;
 					case MULTIPLY:
-						result *= the_number;					
+						result *= opt_.number;					
 						break;
 					case DIVIDE:
-						result /= the_number;
+						result /= opt_.number;
 						break;
 				}
-				break;
-			case ')':
-				if(layer == 0){
-					struct option_float flt;
-					flt.unactivated = 0;
-					flt.number = result;
-					return flt;
-				}
-				layer -= 1;
 				break;
 			default:
 				char* start = arr + i;
@@ -80,12 +73,12 @@ struct option_float process(char* arr, char** ptr, char* changed_ptr){
 				char character = arr[i];
 				arr[i] = '\0';
 				struct option_float opt = process_float(start);
-				struct option_float flt;
 				if(opt.unactivated == 1){
-					flt.unactivated = 1;
-					return flt;
+					return opt;
 				}
 				arr[i] = character;
+				i--;
+				printf("@ %u @", op);
 				float the_number = opt.number;
 				if(op == UNDEFINED){
 					result = the_number;
